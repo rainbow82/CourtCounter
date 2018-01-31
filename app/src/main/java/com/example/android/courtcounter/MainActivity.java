54481b92ad18a192b970e05eb24136c210a4d6f5
Shannon Beach <shannonbeach1982@gmail.com>
2018-01-30 19:23:27 -0500

    added team b layout, reset button. added logic for team b and reset button

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +25,22 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
     public void add_three_points_team_a(View view){
         scoreTeamA += 3;
         displayForTeamA(scoreTeamA);
+    }
+
+    public void add_three_points_team_b(View view){
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
     }
 
     public void add_two_points_team_a(View view){
@@ -34,8 +48,26 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamA(scoreTeamA);
     }
 
+    public void add_two_points_team_b(View view){
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
     public void add_free_throw_team_a(View view){
         scoreTeamA += 1;
         displayForTeamA(scoreTeamA);
     }
+
+    public void add_free_throw_team_b(View view){
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void reset(View view){
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+
 }
